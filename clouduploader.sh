ab97e9f49e3752f64a67742f26a7283e7c87a26e
johnjoker13 <joaoavelino2008@hotmail.com>
2024-06-17 17:35:30 -0300

    uploads to s3

---
+++ clouduploader.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+upload_file_to_bucket() {
+    response=$(aws s3 cp "$1" s3://"$BUCKET_NAME")
+    echo $error_message
+    if [[ $? -ne 0 ]]; then
+      echo "Error while uploading file(s)"
+      echo "Error: $response"
+    else
+      echo "File uploaded successfully to S3 bucket!"
+    fi
+}
+
+for file_path in "$@"; do
+  if [[ ! -f "$file_path" ]]; then
+    echo "File: "$file_path" does not exist or it's a directory."
+  else
+    upload_file_to_bucket "$file_path"
+  fi
+done | pv
