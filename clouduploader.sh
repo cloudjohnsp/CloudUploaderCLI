71fd8858fb415df2ac71b3a87550db8234951d65
cloudjohnsp <jvavelino2017@gmail.com>
2024-06-18 13:44:53 -0300

    adds shareable link generation

--- clouduploader.sh
+++ clouduploader.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+generate_shareable_link() {
+  echo $(aws s3 presign s3://"$BUCKET_NAME"/$(basename "$1") --expires-in 25000)
+}
+
 upload_file_to_bucket() {
-    response=$(aws s3 cp "$1" s3://"$BUCKET_NAME")
-    echo $response
+    local object_name="$1"
+    response=$(aws s3 cp $object_name s3://"$BUCKET_NAME")
     if [[ $? -ne 0 ]]; then
       echo "Error while uploading file(s)"
       echo "Error: $response"
     else
       echo "File uploaded successfully to S3 bucket!"
+      echo -n "Do you want to generate a shareable link?(y/n): "
+      read generate_answer
+      if [[ "$generate_answer" == "y" ]] || [[ "$generate_answer" == "Y" ]]; then
+        generate_shareable_link $object_name
+      fi
     fi
 }
+
 
 for file_path in "$@"; do
   if [[ ! -f "$file_path" ]]; then
@@ -17,4 +27,4 @@ for file_path in "$@"; do
   else
     upload_file_to_bucket "$file_path"
   fi
-done | pv
+done
