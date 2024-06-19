3a47e12e034a73a36c84ae9c3a3cd2983cf11f4b
cloudjohnsp <jvavelino2017@gmail.com>
2024-06-19 12:22:13 -0300

    adds directory upload

--- clouduploader.sh
+++ clouduploader.sh
@@ -6,26 +6,31 @@ generate_shareable_link() {
 
 upload_file_to_bucket() {
     local object_name="$1"
-    response=$(aws s3 cp $object_name s3://"$BUCKET_NAME")
+    if [[ -d "$object_name" ]]; then
+        response=$(aws s3 cp "$object_name" s3://"$BUCKET_NAME"/$(basename "$object_name") --recursive)
+    else
+        response=$(aws s3 cp "$object_name" s3://"$BUCKET_NAME")
+    fi
 
     if [[ $? -ne 0 ]]; then
-      echo "Error while uploading file(s)."
-      echo "Error: $response"
+        echo "Error while uploading file(s)."
+        echo "Error: $response"
     else
-      echo "File uploaded successfully to S3 bucket!"
-      echo -n "Do you want to generate a shareable link?(y/n): "
-      read generate_answer
-      if [[ "$generate_answer" == "y" ]] || [[ "$generate_answer" == "Y" ]]; then
-        generate_shareable_link $object_name
-      fi
+        echo "File uploaded successfully to S3 bucket!"
+        if [[ ! -d "$object_name" ]]; then
+            echo -n "Do you want to generate a shareable link?(y/n): "
+            read generate_answer
+            if [[ "$generate_answer" == "y" ]] || [[ "$generate_answer" == "Y" ]]; then
+                generate_shareable_link "$object_name"
+            fi
+        fi
     fi
 }
 
-
 for file_path in "$@"; do
-  if [[ ! -f "$file_path" ]]; then
-    echo "File: "$file_path" does not exist or is a directory."
-  else
-    upload_file_to_bucket "$file_path"
-  fi
+    if [[ ! -e "$file_path" ]]; then
+        echo "File or directory: "$file_path" does not exist."
+    else
+        upload_file_to_bucket "$file_path"
+    fi
 done
